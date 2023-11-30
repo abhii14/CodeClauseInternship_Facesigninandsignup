5c28834deec19bf16e41def1f935f00f0ecc2721
Abhishek Agrawal <91448483+abhii14@users.noreply.github.com>
2023-11-30 18:06:43 +0530

    first commit

---
+++ face_recognition-master/Model/app.py
@@ -0,0 +1,36 @@
+import cv2 as cv
+
+face_classifier = cv.CascadeClassifier(
+    f"{cv.data.haarcascades}haarcascade_frontalface_default.xml"
+)
+
+video_capture = cv.VideoCapture(0)
+
+
+def detect_bounding_box(vid):
+    gray_image = cv.cvtColor(vid, cv.COLOR_BGR2GRAY)
+    faces = face_classifier.detectMultiScale(gray_image, 1.1, 5, minSize=(40, 40))
+    for (x, y, w, h) in faces:
+        cv.rectangle(vid, (x, y), (x + w, y + h), (0, 255, 0), 5)
+    return faces
+
+
+while True:
+
+    result, video_frame = video_capture.read()  # read frames from the video
+    if result is False:
+        break  # terminate the loop if the frame is not read successfully
+
+    faces = detect_bounding_box(
+        video_frame
+    )  # apply the function we created to the video frame
+
+    cv.imshow(
+        "My Face Detection Project", video_frame
+    )  # display the processed frame in a window named "My Face Detection Project"
+
+    if cv.waitKey(1) & 0xFF == ord("q"):
+        break
+
+video_capture.release()
+cv.destroyAllWindows()
---
+++ face_recognition-master/README.md
@@ -0,0 +1,3 @@
+# Face Recognition
+
+Face recognition with TensorFlow
